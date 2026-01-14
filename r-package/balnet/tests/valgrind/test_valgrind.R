0b2037f732dc09366e9c4260eae5979905f30782
Erik <erik.sverdrup@gmail.com>
2026-01-14 13:58:18 -0800

    add valgrind

---
+++ r-package/balnet/tests/valgrind/test_valgrind.R
@@ -0,0 +1,9 @@
+n <- 100
+p <- 30
+X <- matrix(rnorm(n * p), n, p)
+W <- rbinom(n, 1, 1 / (1 + exp(1 - X[, 1])))
+gr <- list(5:15, 17:19)
+
+fit1 <- balnet(X, W, penalty.factor = runif(ncol(X)))
+
+fit2 <- balnet(X, W, groups = gr, alpha = 0.5)
