6a3f0181547445b6a11847a24cc5156edacbc0ef
Erik <erik.sverdrup@gmail.com>
2026-01-15 15:34:18 -0800

    valgrind cvg

--- r-package/balnet/tests/valgrind/test_valgrind.R
+++ r-package/balnet/tests/valgrind/test_valgrind.R
@@ -6,5 +6,6 @@ W <- rbinom(n, 1, 1 / (1 + exp(1 - X[, 1])))
 gr <- list(5:15, 17:19)
 
 fit1 <- balnet(X, W, penalty.factor = runif(ncol(X)))
+plot(fit1)
 
-fit2 <- balnet(X, W, groups = gr, alpha = 0.5)
+fit2 <- balnet(X, W, groups = gr, alpha = 0.5, standardize = "inplace")
