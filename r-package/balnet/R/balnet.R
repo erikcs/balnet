6573c641ed99580924767179c24efa7e7656e2cc
Erik <erik.sverdrup@gmail.com>
2026-05-15 19:20:41 -0700

    max print / 2 for dual arm print

--- r-package/balnet/R/balnet.R
+++ r-package/balnet/R/balnet.R
@@ -290,14 +290,15 @@ coef.balnet <- function(
 
 #' @rdname coef.balnet
 #' @param x A `coef.balnet.contrast` object.
+#' @param max Maximal number of entries to be printed.
 #' @method print coef.balnet.contrast
 #' @export
-print.coef.balnet.contrast <- function(x, ...) {
+print.coef.balnet.contrast <- function(x, max = getOption("max.print")/2, ...) {
   cat("Control arm coefficients:", "\n")
-  print(x[[1]], ...)
+  print(x[[1]], max = max, ...)
   cat("\n")
   cat("Treated arm coefficients:", "\n")
-  print(x[[2]], ...)
+  print(x[[2]], max = max, ...)
 
   invisible(x)
 }
@@ -690,14 +691,15 @@ balweights.balnet <- function(
 
 #' @rdname balweights
 #' @param x A `balweights.contrast` object.
+#' @param max Maximal number of entries to be printed.
 #' @method print balweights.contrast
 #' @export
-print.balweights.contrast <- function(x, ...) {
+print.balweights.contrast <- function(x, max = getOption("max.print")/2, ...) {
   cat("Control arm weights:", "\n")
-  print(x[[1]], ...)
+  print(x[[1]], max = max, ...)
   cat("\n")
   cat("Treated arm weights:", "\n")
-  print(x[[2]], ...)
+  print(x[[2]], max = max, ...)
 
   invisible(x)
 }
