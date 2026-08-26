19a274df8ec6fec09d43799335d0952c187aec02
Erik <erik.sverdrup@gmail.com>
2026-08-26 16:59:10 +1000

    updt tst

--- r-package/balnet/tests/testthat/test_cv.boot.balnet.R
+++ r-package/balnet/tests/testthat/test_cv.boot.balnet.R
@@ -31,12 +31,21 @@ test_that("cv.boot.balnet is invariant to sample.weights scale", {
   wts <- runif(n)
   seed <- sample.int(1e6, 1)
 
-  set.seed(seed); cv.fit <- cv.boot.balnet(X, W, sample.weights = wts)
-  set.seed(seed); cv.fit.scaled <- cv.boot.balnet(X, W, sample.weights = wts * 42)
-  expect_equal(
-    predict(cv.fit, X),
-    predict(cv.fit.scaled, X)
-  )
+  for (tm in c("imbalance.mean", "imbalance.inf")) {
+    set.seed(seed)
+    cv.fit <- cv.boot.balnet(X, W, sample.weights = wts,
+                             type.measure = tm, B = 20)
+
+    set.seed(seed)
+    cv.fit.scaled <- cv.boot.balnet(X, W, sample.weights = wts * 42,
+                                    type.measure = tm, B = 20)
+
+    expect_equal(
+      predict(cv.fit, X),
+      predict(cv.fit.scaled, X),
+      label = paste0("predict(), type.measure = ", tm)
+    )
+  }
 })
 
 test_that("cv.boot.balnet is invariant to W swap", {
@@ -46,16 +55,24 @@ test_that("cv.boot.balnet is invariant to W swap", {
   W <- rbinom(n, 1, 0.5)
   seed <- sample.int(1e6, 1)
 
-  set.seed(seed); cv.fit <- cv.boot.balnet(X, W)
-  set.seed(seed); cv.fit.swap <- cv.boot.balnet(X, 1 - W)
-  expect_equal(
-    cv.fit$lambda.min$control,
-    cv.fit.swap$lambda.min$treated
-  )
-  expect_equal(
-    cv.fit$lambda.min$treated,
-    cv.fit.swap$lambda.min$control
-  )
+  for (tm in c("imbalance.mean", "imbalance.inf")) {
+    set.seed(seed)
+    cv.fit <- cv.boot.balnet(X, W, type.measure = tm, B = 20)
+
+    set.seed(seed)
+    cv.fit.swap <- cv.boot.balnet(X, 1 - W, type.measure = tm, B = 20)
+
+    expect_equal(
+      cv.fit$lambda.min$control,
+      cv.fit.swap$lambda.min$treated,
+      label = paste0("lambda.min control->treated, type.measure = ", tm)
+    )
+    expect_equal(
+      cv.fit$lambda.min$treated,
+      cv.fit.swap$lambda.min$control,
+      label = paste0("lambda.min treated->control, type.measure = ", tm)
+    )
+  }
 })
 
 test_that("cv.boot.balnet is affine-invariant in X", {
@@ -68,10 +85,17 @@ test_that("cv.boot.balnet is affine-invariant in X", {
   X.aff <- sweep(sweep(X, 2L, a, `*`), 2L, b, `+`)
   seed <- sample.int(1e6, 1)
 
-  set.seed(seed); cv.fit <- cv.boot.balnet(X, W)
-  set.seed(seed); cv.fit.aff <- cv.boot.balnet(X.aff, W)
-  expect_equal(
-    predict(cv.fit, X),
-    predict(cv.fit.aff, X.aff)
-  )
+  for (tm in c("imbalance.mean", "imbalance.inf")) {
+    set.seed(seed)
+    cv.fit <- cv.boot.balnet(X, W, type.measure = tm, B = 20)
+
+    set.seed(seed)
+    cv.fit.aff <- cv.boot.balnet(X.aff, W, type.measure = tm, B = 20)
+
+    expect_equal(
+      predict(cv.fit, X),
+      predict(cv.fit.aff, X.aff),
+      label = paste0("predict(), type.measure = ", tm)
+    )
+  }
 })
